5e955cef441137456613c6627fe5fa5fb540916e
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-10-27 15:16:17 -0400

    version bump to begin working on next release

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'bin'))
 
 require 'aruba/cucumber'
+
+require 'testgen'
